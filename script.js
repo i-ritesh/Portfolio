b6b81b45323a7785ce773ec8dc6f59a33e3d653a
Ritesh Gandalwad <gandalwadritesh90@gmail.com>
2024-01-16 10:21:57 +0530

    Portfolio Completed

--- script.js
+++ script.js
@@ -22,3 +22,21 @@ let resumeLink2 = document.getElementById('resumeLink2').addEventListener('click
     console.log("clik");
     window.open('https://drive.google.com/file/d/1j94nCzfHq7h2-30uFpNghsiFREUAM-y8/view?usp=sharing','_blank')
 })
+
+
+let openMailTo = document.getElementById('openMailTo').addEventListener('click',()=>{
+    let mail = 'gandalwadritesh90@gmail.com'
+    window.location.href='mailto:'+mail
+})
+
+let openContactTo = document.getElementById('openContactTo').addEventListener('click',()=>{
+
+})
+
+let openLinkedinTo = document.getElementById('openLinkedinTo').addEventListener('click',()=>{
+    window.open('https://www.linkedin.com/in/ritesh-gandalwad/','_blank')
+})
+
+let openGithubTo = document.getElementById('openGithubTo').addEventListener('click',()=>{
+    window.open('https://github.com/i-ritesh/','_blank')
+})
